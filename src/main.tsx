06ffdc0894df073e37e80767d76d8c54c2410001
Avinash Gundimeda <avinashgundimeda@gmail.com>
2026-02-07 10:22:59 +0530

    password changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import Lock from "./Lock";
+import Lock from "./Lock";
 
 createRoot(document.getElementById("root")!).render(
+    <Lock>
     <App />
+    </Lock>
 );
